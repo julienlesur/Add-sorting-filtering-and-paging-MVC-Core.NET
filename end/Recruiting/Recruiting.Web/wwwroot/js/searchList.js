614658bc230b7f33f0c28213b7e9dc450f9b0a44
julienlesur <julienlesur@hotmail.fr>
2020-08-24 12:26:19 -0400

    Add a cancel search button

---
+++ end/Recruiting/Recruiting.Web/wwwroot/js/searchList.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $(document).on("keypress", ".input-search-text", function (e) { submitFormWhenEnter(e); });
+    $(document).on("click", ".btn-cancel-search", cancelSearch);
+});
+
+function cancelSearch() {
+    debugger
+    if ($('input.input-search-text').val() != '') {
+        $('input.input-search-text').val('');
+        $('form.form-search').submit();
+    }
+}
+
+function submitFormWhenEnter(e) {
+    debugger;
+    if (e.which === 13) {
+        e.preventDefault();
+        $('form.form-search').submit();
+        return false;
+    }
+}
